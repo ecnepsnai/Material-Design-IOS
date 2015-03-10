693780f397c942ba5a81a5a255a657f82a7ef744
Ian Spence <ian@ecnepsnai.com>
2015-03-09 23:31:46 -0700

    All the Colours!

---
+++ MaterialDesignColours.h
@@ -0,0 +1,50 @@
+//
+//  MaterialDesignColours.h
+//
+//  Created by Ian Spence on 2014-12-21.
+//  Copyright (c) 2014 ecnepsnai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (MaterialDesignColours)
+
++ (UIColor *)MaterialRedWithLevel:(int)level;
++ (UIColor *)MaterialPinkWithLevel:(int)level;
++ (UIColor *)MaterialPurpleWithLevel:(int)level;
++ (UIColor *)MaterialDeepPurpleWithLevel:(int)level;
++ (UIColor *)MaterialIndigoWithLevel:(int)level;
++ (UIColor *)MaterialBlueWithLevel:(int)level;
++ (UIColor *)MaterialLightBlueWithLevel:(int)level;
++ (UIColor *)MaterialCyanWithLevel:(int)level;
++ (UIColor *)MaterialTealWithLevel:(int)level;
++ (UIColor *)MaterialGreenWithLevel:(int)level;
++ (UIColor *)MaterialLightGreenWithLevel:(int)level;
++ (UIColor *)MaterialLimeWithLevel:(int)level;
++ (UIColor *)MaterialYellowWithLevel:(int)level;
++ (UIColor *)MaterialAmberWithLevel:(int)level;
++ (UIColor *)MaterialOrangeWithLevel:(int)level;
++ (UIColor *)MaterialDeepOrangeWithLevel:(int)level;
++ (UIColor *)MaterialBrownWithLevel:(int)level;
++ (UIColor *)MaterialGreyWithLevel:(int)level;
++ (UIColor *)MaterialBlueGreyWithLevel:(int)level;
++ (UIColor *)MaterialRed;
++ (UIColor *)MaterialPink;
++ (UIColor *)MaterialPurple;
++ (UIColor *)MaterialDeepPurple;
++ (UIColor *)MaterialIndigo;
++ (UIColor *)MaterialBlue;
++ (UIColor *)MaterialLightBlue;
++ (UIColor *)MaterialCyan;
++ (UIColor *)MaterialTeal;
++ (UIColor *)MaterialGreen;
++ (UIColor *)MaterialLightGreen;
++ (UIColor *)MaterialLime;
++ (UIColor *)MaterialYellow;
++ (UIColor *)MaterialAmber;
++ (UIColor *)MaterialOrange;
++ (UIColor *)MaterialDeepOrange;
++ (UIColor *)MaterialBrown;
++ (UIColor *)MaterialBlueGrey;
+
+@end
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# Material-Design-IOS
-The Material Design Colours on IOS
+# Material Design IOS
+The Material Design Colours on IOS in Objective-C.  It has all of the colours (except Black and White) [seen here](http://www.google.com/design/spec/style/color.html#color-color-palette).
+
+## Usage
+
+MDI is really easy to use, just import:
+
+    #import "MaterialDesignColours.h"
+    
+And use it:
+    
+    UIColor * MyColor = [UIColor MaterialRed];
+    
+The level of the colour is optional, if you don't specify a level it will return 500.  Otherwise you can specify a specific level using:
+
+    UIColor * MyColor = [UIColor MaterialRedWithLevel:200];
